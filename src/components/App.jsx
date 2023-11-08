12e17da61e449e5be3288898396e696a5fce38d3
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-11-08 20:48:21 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,5 @@
+import { AppWrapper } from './Helpers/Components.styled';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return <AppWrapper>React homework template</AppWrapper>;
 };
--- src/components/Helpers/Components.styled.js
+++ src/components/Helpers/Components.styled.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
 
+const AppWrapper = styled.div`
+  height: '100vh';
+  display: 'flex';
+  justify-content: 'center';
+  align-items: 'center';
+  font-size: 40px;
+  color: '#010101';
+`;
+
 const Div = styled.div``;
 
 const Ul = styled.ul``;
 
-export { Div, Ul };
+export { AppWrapper, Div, Ul };
