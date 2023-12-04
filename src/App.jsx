2623cc8a0fe611232d020e8f00205cd04fe8698e
Br0kEnM1Nd-Git <mrgamer594@gmail.com>
2023-12-04 23:56:36 +0200

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-import { AppWrapper } from './components/Helpers/Components.styled';
+import { Suspense } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 export const App = () => {
-  return <AppWrapper>React template</AppWrapper>;
+  return (
+    <Suspense fallback={<div>loading...</div>}>
+      <Routes>
+        <Route path="/" element={<div>Layout</div>}>
+          <Route index element={<div>Index</div>} />
+          <Route path="test" element={<div>Test</div>} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Suspense>
+  );
 };
